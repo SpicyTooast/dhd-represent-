187426946937c5c2d52412513b8a7a016d8c2314
Spicy Toast <116366898+SpicyTooast@users.noreply.github.com>
2022-10-21 21:53:19 +0530

    Difference

--- delta.java
+++ delta.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("HELLO WORLD?");
         //hii
+       System.out.print("Hey there");
+        // difference between println and print
     }
 }
